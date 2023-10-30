f446a6710769985d86b69cbf820274e70e33707d
Jonathan ASSAMAGAN <80401029+kuro-jojo@users.noreply.github.com>
2023-10-30 17:31:24 +0000

    added installed packages+script

---
+++ installed_packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if the file exists
+if [ -f installed_packages.txt ]; then
+    # Read the list of packages and reinstall them
+    while read package; do
+        sudo apt-get install --reinstall "$package"
+    done < installed_packages.txt
+else
+    echo "The file 'installed_packages.txt' does not exist."
+fi
